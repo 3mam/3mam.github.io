1f3cbcd63791a64b81f615037caa43c39a79aeca
Konrad Chmielecki <6963493+3mam@users.noreply.github.com>
2021-09-05 11:11:02 +0200

    Add files via upload

---
+++ app/sw.js
@@ -0,0 +1,8 @@
+// Listen for install event, set callback
+self.addEventListener('install', (event) => {
+  // Perform some task
+})
+
+self.addEventListener('activate', (event) => {
+  // Perform some task
+});
